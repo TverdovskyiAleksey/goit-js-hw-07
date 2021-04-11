6599c45f2707ca4e3c1095e0b36382fb9a03fc54
Alex <tverdovskyi92@gmail.com>
2021-04-11 09:45:26 +0300

    delete comments

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,3 @@
-// const inputEl = document.querySelector("#validation-input");
-
-// inputEl.addEventListener('blur', onInputBlur);
-
-// // function onInputBlur() {
-// //     console.log(this.value.length);
-    
-// //     if (this.getAttribute('data-length') > this.value.length) { 
-// //     this.classList.add('invalid');
-// //     } else {
-// //     this.classList.remove('invalid');
-// //     this.classList.add('valid');
-// //     }
-// // };
-
 const inputEl = document.querySelector('#validation-input');
 
 const validationInput = function () {
