eb5d340a9554e0db50497d129098a27be42fff47
Alex <tverdovskyi92@gmail.com>
2021-04-11 09:35:49 +0300

    change task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,27 @@
-const inputEl = document.getElementById("validation-input");
+// const inputEl = document.querySelector("#validation-input");
 
-inputEl.addEventListener('blur', onInputBlur);
+// inputEl.addEventListener('blur', onInputBlur);
 
-function onInputBlur() {
-    console.log(this.value.length);
+// // function onInputBlur() {
+// //     console.log(this.value.length);
     
-    if (this.getAttribute('data-length') > this.value.length) { 
-    this.classList.add('invalid');
-    } else {
-    this.classList.remove('invalid');
-    this.classList.add('valid');
-    }
+// //     if (this.getAttribute('data-length') > this.value.length) { 
+// //     this.classList.add('invalid');
+// //     } else {
+// //     this.classList.remove('invalid');
+// //     this.classList.add('valid');
+// //     }
+// // };
+
+const inputEl = document.querySelector('#validation-input');
+
+const validationInput = function () {
+    console.log(this.value.length);
+  if (inputEl.value.length === Number(inputEl.dataset.length)) {
+    inputEl.classList = 'valid';
+  } else {
+    inputEl.classList = 'invalid';
+  }
 };
+
+inputEl.addEventListener('blur', validationInput);
